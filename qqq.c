2576037f15f2d03f8cdaff4de3531af3dfea6037
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-01-24 16:41:20 +0530

    Create qqq.c

---
+++ qqq.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,j=0,k=0;
+printf("enter the number");
+scanf("%d",&n);
+scanf("%d",&k);
+for(i=n;i,=k;i++)
+{e
+if(i%2==0)
+{e
+e[j]=i;
+j++;e
+}
+else
+{e
+0[k]=i;
+k++;
+}
+for(i=0;i<j;i++)
+{
+printf("%d",e[i]);
+}
+return 0;
+}
