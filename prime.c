42296a44a8b85a7ebff7c50139452324fd9fb4d7
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-01-31 16:19:07 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main()
 {
-int low, high, i, flag;
+int low,high,i,flag;
 printf("Enter two numbers(intervals): ");
-scanf("%d %d", &low, &high);
+scanf("%d %d",&low,&high);
 printf("Prime numbers between %d and %d are: ", low, high);
-while (low < high)
+while (low<high)
 {
 flag = 0;
-for(i = 2; i <= low/2; ++i)
+for(i=2;i<=low/2;++i)
 {
-if(low % i == 0)
+if(low % i==0)
 {
-flag = 1;
+flag=1;
 break;
 }
 }
-if (flag == 0)
-printf("%d ", low);
+if(flag==0)
+printf("%d",low);
 ++low;
 }
 return 0;
