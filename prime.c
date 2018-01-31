f39f2e0206487bff12922419e2fc3402eb915052
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-01-31 16:17:34 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+int main()
 {
-int low,high,i,n;
-printf("enter two number(interval)");
-scanf("%d %d",&low,&high);
-printf("prime number between %d and %d are",low,high);
-while(low<high)
+int low, high, i, flag;
+printf("Enter two numbers(intervals): ");
+scanf("%d %d", &low, &high);
+printf("Prime numbers between %d and %d are: ", low, high);
+while (low < high)
 {
-flag=0;
-for(i=2;i<=low/2;i++)
+flag = 0;
+for(i = 2; i <= low/2; ++i)
 {
-if(low % i==0)
+if(low % i == 0)
 {
-flag=1;
+flag = 1;
 break;
 }
 }
-if (flag==0)
-printf("%d",low);
+if (flag == 0)
+printf("%d ", low);
 ++low;
 }
 return 0;
